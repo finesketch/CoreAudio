6ee4e9cb8e595f96cde9f488a47b07cf10c97ed2
finesketch <finesketch8@gmail.com>
2015-07-16 00:15:28 -1000

    Initial project for ProcessAudioStreamData
    Initial project for ProcessAudioStreamData

---
+++ ProcessAudioStreamData/ProcessAudioStreamData/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ProcessAudioStreamData
+//
+//  Created by Kyle Chew on 7/16/15.
+//  Copyright (c) 2015 FineSketch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
